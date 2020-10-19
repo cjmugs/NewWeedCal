15d9b6bd563b5557f12d3d4ac2ebae4bcde8a3ac
cjmugs <cjmugs@gmail.com>
2020-10-19 12:15:51 -0500

    first

---
+++ NewWeedCal.py
@@ -0,0 +1,16 @@
+# Item Cost
+num = float(input("Enter a Purchase Price: "))
+
+# loyalty Points
+num_2 = float(input("Enter Loyalty Points: "))
+
+# THC Percentage
+num_3 = float(input("Enter THC: "))
+
+# Veteran Discount
+vet_disc = float(num * .10)
+
+# Taxes to be collected
+weed_tax_1 = float(num * .0789)
+weed_tax_2 = float(num * .0705)
+weed_tax_3 = float(num * .04)
