b3da4de18a9401b33166e30a35952dd901dc07dd
cjmugs <cjmugs@gmail.com>
2020-10-19 14:33:28 -0500

    updates

--- NewWeedCal.py
+++ NewWeedCal.py
@@ -11,23 +11,22 @@ vet_disc = float(num * .10)
 weed_tax_1 = float(num * .0789)
 weed_tax_2 = float(num * .0705)
 weed_tax_3 = float(num * .04)
+weed_tax_4 = float(num * .10)
 
 # Calculations
 result_1 = num - vet_disc
 result_2 = result_1 + weed_tax_1
 tax_total = weed_tax_1 + weed_tax_2 + weed_tax_3
+tax_total_2 = weed_tax_1 + weed_tax_2 + weed_tax_3 + weed_tax_4
 total = result_1 + weed_tax_1 + weed_tax_2 + weed_tax_3 - num_2
 total_2 = weed_tax_1 + weed_tax_2 + weed_tax_3 + num
 
-# if statement for totals
+# if statement for percentage totals
 if(num_3 <= 35):
-    print("TRUE")
+    print("Total Taxes for less than 35% = ","$", tax_total)
 else:
-    print("FALSE")
+    print("Total Taxes for greater than 35% = ","$", tax_total_2)
 
 print("Veteran Discount = ","$", vet_disc)
-print("Illinois Rec Tax = ","$", weed_tax_1)
-print("Rec Cannabis Tax = ","$", weed_tax_2 + weed_tax_3)
-print("Total taxes collected = ","$", tax_total)
 print("Total without discounts", "$", total_2 )
 print("Total with discounts = ","$", total)
