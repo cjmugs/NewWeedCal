f2ddca2fdc4248f288bb144392d95726c9fc18e1
cjmugs <cjmugs@gmail.com>
2020-10-19 15:33:59 -0500

    updates

--- NewWeedCal.py
+++ NewWeedCal.py
@@ -13,20 +13,22 @@ weed_tax_2 = float(num * .0705)
 weed_tax_3 = float(num * .04)
 weed_tax_4 = float(num * .10)
 
-# Calculations
-result_1 = num - vet_disc
-result_2 = result_1 + weed_tax_1
 tax_total = weed_tax_1 + weed_tax_2 + weed_tax_3
 tax_total_2 = weed_tax_1 + weed_tax_2 + weed_tax_3 + weed_tax_4
-total = result_1 + weed_tax_1 + weed_tax_2 + weed_tax_3 - num_2
-total_2 = weed_tax_1 + weed_tax_2 + weed_tax_3 + num
+
+# Sums of totals
+
 
 # if statement for percentage totals
 if(num_3 <= 35):
     print("Total Taxes for less than 35% = ","$", tax_total)
+    print("Total with taxes and discounts = ","$",)
+    print("Total without taxes and discounts = ","$",)
 else:
     print("Total Taxes for greater than 35% = ","$", tax_total_2)
+    print("Total with taxes and discounts = ","$",)
+    print("Total without taxes and discounts = ","$",)
 
-print("Veteran Discount = ","$", vet_disc)
-print("Total without discounts", "$", total_2 )
-print("Total with discounts = ","$", total)
+#print("Veteran Discount = ","$", vet_disc)
+#print("Total without discounts", "$", total_2 )
+#print("Total with discounts = ","$", total)
